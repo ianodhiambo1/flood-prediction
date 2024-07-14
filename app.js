34b6088d8b13e25b192578cfa925a74e8f81c9c8
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2024-07-15 01:10:53 +0300

    metrologist page admin page

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const connection = mysql.createConnection({
-	host     : 'localhost',
-    port     : '3308',
-	user     : 'root',
-	password : 'root',
-	database : 'flood'
-});
+require('dotenv').config();
+const fs = require('fs');
+// Parse the database URL
+const uri = process.env.DATABASE_URI;
+const fields = new URL(uri);
+
+// Build the connection configuration
+const connectionConfig = {
+  host: fields.hostname,
+  port: fields.port,
+  user: fields.username,
+  password: fields.password,
+  database: 'flood',
+  ssl: {
+    ca: fs.readFileSync(process.env.SSL_CA_PATH)
+  }
+};
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use(
   session({
@@ -21,6 +35,7 @@ app.use(
     saveUninitialized: true,
   })
 );
+const connection = mysql.createPool(connectionConfig);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -33,6 +48,13 @@ app.get("/", function (request, response) {
 app.get("/login", function (request, response) {
   response.sendFile(path.join(__dirname, "views", "/login.html"));
 });
+app.get("/admin-login", function (request, response) {
+  response.sendFile(path.join(__dirname, "views", "/admin-login.html"));
+});
+app.get("/met-login", function (request, response) {
+  response.sendFile(path.join(__dirname, "views", "/met-login.html"));
+});
+
 app.get("/signup", function (request, response) {
   response.sendFile(path.join(__dirname, "views", "/signup.html"));
 });
@@ -41,6 +63,20 @@ app.get("/home", function (request, response) {
     response.render("plain");
   } else {
     response.redirect("/login");
+  }
+});
+app.get("/admin-home", function (request, response) {
+  if (request.session.loggedin) {
+    response.sendFile(path.join(__dirname, "views", "/admin-home.html"));
+  } else {
+    response.sendFile(path.join(__dirname, "views", "/admin-login.html"));
+  }
+});
+app.get("/met-home", function (request, response) {
+  if (request.session.loggedin) {
+    response.sendFile(path.join(__dirname, "views", "/met-home.html"));
+  } else {
+    response.sendFile(path.join(__dirname, "views", "/met-login.html"));
   }
 });
 app.get('/map', function(request, response) {
@@ -100,6 +136,91 @@ app.get("/logout", (req, res) => {
     }
   });
 });
+app.post("/auth-admin", function (request, response) {
+  let username = request.body.username;
+  let password = request.body.password;
+
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM admin WHERE Username = ?",
+      [username],
+      function (error, results, fields) {
+        if (error) {
+          console.error("Database query error:", error);
+          return response.status(500).send("Internal Server Error");
+        }
+
+        if (results.length > 0) {
+          bcrypt.compare(password, results[0].Password, function (err, result) {
+            if (err) {
+              console.error("Error comparing passwords:", err);
+              return response.status(500).send("Internal Server Error");
+            }
+            if (result) {
+              request.session.loggedin = true;
+              request.session.username = username;
+              request.session.email = results[0].Email;
+              return response.redirect("/admin-home");
+            } else {
+              return response.send("Incorrect Username and/or Password!");
+            }
+          });
+        } else {
+          return response.send("Incorrect Username and/or Password!");
+        }
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+  }
+});
+app.post("/auth-met", function (request, response) {
+  let username = request.body.username;
+  let password = request.body.password;
+
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM metreologist WHERE Username = ?",
+      [username],
+      function (error, results, fields) {
+        if (error) {
+          console.error("Database query error:", error);
+          return response.status(500).send("Internal Server Error");
+        }
+
+        if (results.length > 0) {
+          bcrypt.compare(password, results[0].Password, function (err, result) {
+            if (err) {
+              console.error("Error comparing passwords:", err);
+              return response.status(500).send("Internal Server Error");
+            }
+            if (result) {
+              request.session.loggedin = true;
+              request.session.username = username;
+              request.session.email = results[0].Email;
+              return response.redirect("/met-home");
+            } else {
+              return response.send("Incorrect Username and/or Password!");
+            }
+          });
+        } else {
+          return response.send("Incorrect Username and/or Password!");
+        }
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+  }
+});
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Could not log out");
+    } else {
+      res.redirect("/admin-login");
+    }
+  });
+});
 
 app.post("/register", function (request, response) {
   let username = request.body.username;
@@ -125,6 +246,32 @@ app.post("/register", function (request, response) {
         function (error, results, fields) {
           if (error) throw error;
           response.redirect("/login");
+        }
+      );
+    });
+  } else {
+    response.send("Please fill all the fields!");
+    response.end();
+  }
+});
+app.get("/register-met", function (request, response) {
+  let id=1;
+  let username = "met1";
+  let password = "met1";
+
+
+  if (
+    username &&
+    password
+  ) {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (err) throw err;
+      connection.query(
+        "INSERT INTO metreologist (metid, Username, Password) VALUES (?,?, ?)",
+        [id, username, hash],
+        function (error, results, fields) {
+          if (error) throw error;
+          response.redirect("/met-login");
         }
       );
     });
@@ -330,32 +477,117 @@ app.get("/notification", (req, res) => {
     res.redirect("/home");
   });
 });
-//   app.locals.getIconSvg = (floodPrediction) => {
-//     let svgColor = '#00a4cd'; // Default color
 
-//     if (floodPrediction < 0.3) {
-//       svgColor = '#f18700';
-//     } else if (floodPrediction < 0.7) {
-//       svgColor = '#ffff00'; // Change to your desired color for this range
-//     }
 
-//     return `
-//       <svg role="img" focusable="false" width="38" height="38" viewBox="0 0 38 38" fill-rule="evenodd"
-//         stroke-linejoin="round" stroke-miterlimit="1.41421">
-//         <title>Flood alert</title>
-//         <path d="M19.037 2.791c.69.018 1.345.408 1.689 1.007l15.353 28.466c.602 1.188-.261 2.848-1.722 2.883H3.65c-1.335-.032-2.377-1.591-1.721-2.883L17.282 3.798a1.95 1.95 0 0 1 1.755-1.007z"
-//           fill="${svgColor}"></path>
-//         <path d="M19.083 1.064h-.032c-1.435-.011-2.6.71-3.268 1.875l-.015.027L.402 31.456l-.014.027c-1.225 2.416.723 5.331 3.22 5.391l.028.001h30.735l.028-.001c2.732-.066 4.347-3.169 3.22-5.391l-.014-.027-15.366-28.49a.6.6 0 0 0-.015-.026c-.639-1.115-1.858-1.842-3.141-1.876zm-.046 1.727c.69.018 1.345.408 1.689 1.007l15.353 28.466c.602 1.188-.261 2.848-1.722 2.883H3.65c-1.335-.032-2.377-1.591-1.721-2.883L17.282 3.798a1.95 1.95 0 0 1 1.755-1.007zm-.36 2.706a.37.37 0 0 1 .327-.195c.136 0 .262.075.327.195l14.6 27.089c.062.116.059.255-.008.367s-.189.181-.319.181h-29.2a.37.37 0 0 1-.327-.548l14.6-27.089z"
-//           fill="#fff"></path>
-//         <path d="M16.309 17.718v1.196l2.116-2.434c.124-.142.305-.224.494-.224s.369.083.493.226l6.125 7.081h.001c.235.273.205.685-.067.921s-.686.206-.922-.067l-.559-.646v3.725c-.192.015-.286.066-.454.171-.188.116-.436.345-.839.531a2.36 2.36 0 0 1-.978.203h-.027a2.34 2.34 0 0 1-1.291-.399c-.306-.204-.49-.362-.652-.432a.89.89 0 0 0-.395-.079c-.26.006-.357.056-.55.176s-.435.345-.839.531c-.268.122-.602.204-.977.203-.547.004-.997-.192-1.306-.388l-.667-.442c-.101-.048-.164-.066-.271-.068-.205-.009-.557.136-.958.356v-4.052l-.561.645a.65.65 0 0 1-.494.225c-.151 0-.304-.053-.428-.16-.272-.237-.301-.65-.064-.922l2.542-2.923v-2.954h1.528z"
-//           fill="#181c1b"></path>
-//         <path d="M21.054 23.852h1.954v2.15h-1.954z" fill="#fff"></path>
-//         <path d="M32.292 31.961H5.708l1.437-2.652h.422c.286.243.638.519 1.009.757.214.137.434.262.663.36a1.94 1.94 0 0 0 .761.179c.751-.019 1.299-.38 1.826-.647.521-.285.995-.491 1.316-.481.162.001.272.034.415.103.212.1.475.322.855.562s.906.468 1.561.463a2.8 2.8 0 0 0 1.165-.24c.481-.22.786-.499 1.036-.656.257-.16.421-.241.785-.248a1.3 1.3 0 0 1 .568.116c.234.1.472.307.844.553.366.244.902.482 1.573.475.452.001.846-.096 1.166-.24.482-.22.786-.499 1.037-.656.257-.16.421-.242.784-.248.246-.003.537.101.757.222.11.06.202.121.262.165l.067.05.059.047c.372.277.969.66 1.807.66h.034c.76-.014 1.408-.365 1.967-.715a7.58 7.58 0 0 0 .725-.53l.062-.051h.188l1.433 2.652z"
-//           fill="${svgColor}"></path>
-//         <path d="M19.037 3.009c.69.018 1.345.408 1.689 1.007l15.353 28.466c.602 1.188-.261 2.848-1.722 2.883H3.65c-1.335-.032-2.377-1.591-1.721-2.883L17.282 4.016a1.95 1.95 0 0 1 1.755-1.007z"
-//           fill="none" stroke="#f18700" stroke-width="1.15"></path>
-//       </svg>
-//     `;
-//   };
+// Fetch users
+app.get('/users', (req, res) => {
+    let sql = 'SELECT * FROM users';
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(results);
+    });
+});
+app.get('/users/:id', (req, res) => {
+    let sql = `SELECT * FROM users WHERE UserID = ${req.params.id}`;
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(results);
+    });
+});
 
+// Create user
+app.post('/users', (req, res) => {
+    let newUser = req.body;
+    let sql = 'INSERT INTO users SET ?';
+  connection.query(sql, newUser, (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        
+        res.send(result);
+    });
+});
+
+// Edit user
+app.put('/users/:id', (req, res) => {
+    let updatedUser = req.body;
+    let sql = `UPDATE users SET ? WHERE UserID = ${req.params.id}`;
+    connection.query(sql, updatedUser, (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+// Delete user
+app.delete('/users/:id', (req, res) => {
+    let sql = `DELETE FROM users WHERE UserID = ${req.params.id}`;
+    connection.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(result);
+    });
+});
+
+// Fetch sensor data
+app.get('/sensor-data', (req, res) => {
+  let sql = 'SELECT * FROM sensordata';
+  connection.query(sql, (err, results) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+      res.send(results);
+  });
+});
+
+app.get('/sensor-data/:id', (req, res) => {
+  let sql = `SELECT * FROM sensordata WHERE SensorDataID = ${req.params.id}`;
+  connection.query(sql, (err, results) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+      res.send(results);
+  });
+});
+
+// Create sensor data
+app.post('/sensor-data', (req, res) => {
+  let newSensorData = req.body;
+  let sql = 'INSERT INTO sensordata SET ?';
+  connection.query(sql, newSensorData, (err, result) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+      res.send(result);
+  });
+});
+
+// Edit sensor data
+app.put('/sensor-data/:id', (req, res) => {
+  let updatedSensorData = req.body;
+  let sql = `UPDATE sensordata SET ? WHERE SensorDataID = ${req.params.id}`;
+  connection.query(sql, updatedSensorData, (err, result) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+      res.send(result);
+  });
+});
+
+// Delete sensor data
+app.delete('/sensor-data/:id', (req, res) => {
+  let sql = `DELETE FROM sensordata WHERE SensorDataID = ${req.params.id}`;
+  connection.query(sql, (err, result) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+      res.send(result);
+  });
+});
 app.listen(port, () => console.log(`App listening on port ${port}!`));
